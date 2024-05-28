390672741f2ac9f66ae4a52415ddf5add8769ef8
EKENG <114169719+leukeng@users.noreply.github.com>
2024-05-28 20:31:24 +0100

    upload

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,23 @@
+export default asyncUploadUser;
+
+import { uploadPhoto, createUser } from "./utils.js";
+
+async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+// export default asyncUploadUser;
